1ac155a257213a5a3b8b5b84e5468ca211073194
N4l4N <165954429+N4l4N@users.noreply.github.com>
2024-04-25 16:16:38 -0300

    projeto de site LAPS

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>ProjetoLAPS</h1>
+<p>Não fazia ideia do que usar de tema para esse site, decidi fazer um site sobre mim mesmo.</p>
+<p>Grandes chances de que isso fique pior do que eu imagino.</p>
+
---
+++ script.js
@@ -0,0 +1,20 @@
+function ps(){
+    event.preventDefault()
+    window.scrollBy(0,100)
+}
+function lv(){
+    event.preventDefault()
+    window.scrollBy(0,300)
+}
+function sr(){
+    event.preventDefault()
+    window.scrollBy(0,900)
+}
+function fm(){
+    event.preventDefault()
+    window.scrollBy(0,1300)
+}
+function jg(){
+    event.preventDefault()
+    window.scrollBy(0,1800)
+}
